3eb69c4dc264e5fb5735cd58770c5f0ef8d36b63
Zach <zach@codehooker.com>
2017-09-19 16:19:58 +0000

    adds the basic bootstrap3 modal instead of jquery due to css limitations on cytube.

--- chan.js
+++ chan.js
@@ -1,19 +1,7 @@
-var setupscript = document.createElement('script');
-if (!jQuery){
-    setupscript.src = '//code.jquery.com/jquery-3.2.1.min.js';
-    document.getElementsByTagName('head')[0].appendChild(setupscript); 
-}
-if (!jQuery.ui){
-    setupscript.src = '//code.jquery.com/ui/1.12.1/jquery-ui.min.js';
-    document.getElementsByTagName('head')[0].appendChild(setupscript); 
-}
 $(function(){
-$('<a/>', {
-    text: 'zimJS',
-    href: '#',
-    click: function(){$("<p>Direct interaction between slutbot or some channel information.. etc!</p>").dialog();}
-}).wrap('<li />').parent().appendTo('.navbar ul');
-
+  var r = '<div class="modal fade" id="zimbasic" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"> <div class="modal-dialog" role="document"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> <h4 class="modal-title" id="myModalLabel">Modal title</h4> </div><div class="modal-body"> ... </div><div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary">Save changes</button> </div></div></div></div>';
+  $("body").append(r);
+  
+  r= $('<li><a href="#zimbasic" class="btn btn-red" data-toggle="modal" data-target="#zimbasic">ZimJS</a></li>');
+  $(".navbar ul").append(r);
 });
-console.log(socket)
-console.log('Initialized userscript Cytujs: Channel JS')
